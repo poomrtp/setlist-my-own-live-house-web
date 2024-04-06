e41b64671e9071e762222008e202901cb177509e
Ratthapon Sananaur <poom8165@gmail.com>
2024-04-06 17:44:26 +0700

    feat: add meta data

--- src/app/most-listened/page.tsx
+++ src/app/most-listened/page.tsx
@@ -1,8 +1,19 @@
 import MostListenedContainer from '@/components/most-listened/mostListenedContainer';
+import Head from 'next/head';
 import React from 'react';
 
 function MostListenedPage() {
-  return <MostListenedContainer />;
+  return (
+    <>
+      <Head>
+        <title>My own Live House</title>
+        <meta property="og:title" content="My own Live House" key="title" />
+        <meta name="keywords" content="setlist, spotify, music," />
+        <meta name="author" content="Ratthapon Sananaur" />
+      </Head>
+      <MostListenedContainer />
+    </>
+  );
 }
 
 export default MostListenedPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import SpotifyBtn from '@/components/buttons/spotifyBtn';
 import { useSession } from 'next-auth/react';
+import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -18,31 +19,39 @@ export default function Home() {
   }, [session, router]);
 
   return (
-    <div className="w-full h-full">
-      <div className="hero">
-        <div className="hero-content text-center flex-col lg:flex-row">
-          <div>
-            <h1 className="text-5xl font-bold">
-              Welcome to My Spotify data app
-            </h1>
-            {session?.status === 'authenticated' ? (
-              <>
-                <p className="py-6">{`Let's create your set list`}</p>
-                <Link href={'/most-listened'}>
-                  <button className="btn btn-primary">Get Started</button>
-                </Link>
-              </>
-            ) : (
-              <>
-                <p className="py-6">Sign in to get started</p>
-                <div className="flex justify-center">
-                  <SpotifyBtn />
-                </div>
-              </>
-            )}
+    <>
+      <Head>
+        <title>My own Live House</title>
+        <meta property="og:title" content="My own Live House" key="title" />
+        <meta name="keywords" content="setlist, spotify, music," />
+        <meta name="author" content="Ratthapon Sananaur" />
+      </Head>
+      <div className="w-full h-full">
+        <div className="hero">
+          <div className="hero-content text-center flex-col lg:flex-row">
+            <div>
+              <h1 className="text-5xl font-bold">
+                Welcome to My Spotify data app
+              </h1>
+              {session?.status === 'authenticated' ? (
+                <>
+                  <p className="py-6">{`Let's create your set list`}</p>
+                  <Link href={'/most-listened'}>
+                    <button className="btn btn-primary">Get Started</button>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <p className="py-6">Sign in to get started</p>
+                  <div className="flex justify-center">
+                    <SpotifyBtn />
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
